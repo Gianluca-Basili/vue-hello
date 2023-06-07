f87c2567be54f3b9da172e69023f749074919eed
Gianluca-Basili <128229910+Gianluca-Basili@users.noreply.github.com>
2023-06-07 15:02:32 +0200

    esercizio finito

---
+++ main.js
@@ -0,0 +1,12 @@
+ const {createApp} = Vue;
+
+ createApp({
+
+    data() {
+        return {
+            html:'<h1>Benvenuto vue js </h1>',
+            image:'https://lh5.googleusercontent.com/GNpTwYOKksjpw8ThU5e2fuTblBGoBh2o5mqbQxF3d0xNQovvi9jEiLY1EPssVJOQ1tQzlpJ45UuxusULC0LVQn-DlV6j4Xq2tZiv-GPBTBulP26EHDub89uOYvZvIPUli1PQbGJmsLfQGtoZFA'
+        }
+    },
+
+ }).mount('#app');
---
+++ readme.md
@@ -0,0 +1,4 @@
+PROBLEMA:Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+1- inserisco lo script di vuejs 
+2- preparo la struttura di vuejs nel dom
